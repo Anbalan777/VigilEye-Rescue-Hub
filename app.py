ec9b61834564ba37d7e872f4fb514d193c2a9531
Anbalan <140716526+Anbalan777@users.noreply.github.com>
2025-06-18 09:33:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -279,5 +279,5 @@ def get_accident_history():
     
     return jsonify(mock_history)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
